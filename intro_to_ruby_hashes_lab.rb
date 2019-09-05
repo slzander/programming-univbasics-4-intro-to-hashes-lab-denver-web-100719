b84287bbb213e9532ae331d9ac99e984949580c5
Stacey Zander <staceyzander@gmail.com>
2019-09-05 18:27:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,8 +24,10 @@ end
 
 def update_counting_hash(hash, key)
   if hash[key]
-    hash[key] += hash[key] + 1 
+    hash[key] += hash[key] + 1
+    return hash
   else
-    hash[key] = 1 
+    hash[key] = 1
+    return hash
   end 
 end
